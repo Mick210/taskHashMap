a5bd6136524090f6d9221df22d34faf8f871d948
Mick210 <sid222m@gmail.com>
2025-05-26 01:24:17 +0300

    task done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
 
+         /*Задача
+        Дан массив пар названий книг и авторов,
+        необходимо привести каждую пару в строку вида:
+        «Название книги» Имя Отчество Фамилия автора.*/
 
+        HashMap<String, String> nameBookAuthor = new HashMap<>();
+
+        nameBookAuthor.put("Война и мир", "Л.Н. Толстой");
+        nameBookAuthor.put("Мертвые души", "Н.В. Гоголь");
+        nameBookAuthor.put("Ревизор", "Н.В. Гоголь");
+        nameBookAuthor.put("Сказка о рыбаке и рыбке", "А.С. Пушкин");
+
+        nameBookAuthor.forEach((key, value) -> System.out.println("\"" + key + "\" " + value));
     }
 }
