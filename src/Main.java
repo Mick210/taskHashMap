b543933a38c2f37d8311ee0c108105dcb5273131
Mick210 <sid222m@gmail.com>
2025-06-07 20:41:00 +0300

    add an increment

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
+import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Main {
     public static void main(String[] args) {
 
+      /*Задача
+    Дан массив пар названий книг и авторов,
+    необходимо привести каждую пару в строку вида:
+    «Название книги» Имя Отчество Фамилия автора.*/
 
+        HashMap<String, String> nameBookAuthor = new HashMap<>();
+
+        nameBookAuthor.put("Война и мир", "Л.Н. Толстой");
+        nameBookAuthor.put("Мертвые души", "Н.В. Гоголь");
+        nameBookAuthor.put("Ревизор", "Н.В. Гоголь");
+        nameBookAuthor.put("Сказка о рыбаке и рыбке", "А.С. Пушкин");
+
+        AtomicInteger i = new AtomicInteger();
+        nameBookAuthor.forEach((key, value) -> {
+            i.getAndIncrement();
+            System.out.println(i + ". \"" + key + "\" " + value);
+        });
     }
 }
